44a9353404e2576b1af006d86bf30fa40c5721a7
Petri-Johan Last <petri.last@sourcegraph.com>
2023-12-06 10:17:02 +0200

    Get tailwind working seamlessly

---
+++ assets/tailwind.config.js
@@ -0,0 +1,16 @@
+// See the Tailwind configuration guide for advanced usage
+// https://tailwindcss.com/docs/configuration
+
+module.exports = {
+  content: [
+    "./**/*.eml.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#FD4F00",
+      }
+    },
+  },
+  plugins: []
+}
--- lib/router.ml
+++ lib/router.ml
@@ -1,4 +1,8 @@
 open Dream
 
-let router =
-  [get "/" @@ Controllers.Home.index; get "/**" @@ static "assets"]
+let loader _root path _request =
+    match Tailwindcss.read path with
+    | None -> Dream.empty `Not_Found
+    | Some asset -> Dream.respond asset
+
+let router = [get "/" @@ Controllers.Home.index; get "/css/**" @@ static ~loader ""; get "/**" @@ static "assets"]
